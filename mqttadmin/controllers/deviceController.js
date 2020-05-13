4b1c85f06ae08cd7a6b7831da564674b21a1413b
András Maróy <andras@maroy.hu>
2020-05-13 18:09:32 +0200

    Implement kafka/scram related changes

--- mqttadmin/controllers/deviceController.js
+++ mqttadmin/controllers/deviceController.js
@@ -1,5 +1,6 @@
 var async = require('async')
 var mosquittoPBKDF2 = require('mosquitto-pbkdf2');
+var zookeeper = require('node-zookeeper-client');
 
 var Device = require('../models/device');
 
@@ -7,52 +8,27 @@ const { body,validationResult } = require('express-validator/check');
 const { sanitizeBody } = require('express-validator/filter');
 
 exports.index = function(req, res) {
-    //res.send('NOT IMPLEMENTED: Site Home Page');
-
-    async.parallel({
-        device_count: function(callback) {
-            Device.count({}, callback); // Pass an empty object as match condition to find all documents of this collection
-        },
-    }, function(err, results) {
-        res.render('index', { title: 'MQTT Admin', error: err, data: results });
-    });
-
+    Device.count().then((result) => {
+        res.render('index', { title: 'MQTT Admin', data: { device_count: result }})
+    })
 };
 
 // Display list of all Devices.
 exports.device_list = function(req, res) {
-    //res.send('NOT IMPLEMENTED: Device list');
-
-    Device.find({})
-        .exec(function (err, list_devices) {
-            if (err) { return next(err); }
-            //Successful, so render
-            res.render('device_list', { title: 'Device List', device_list: list_devices });
-        });
+    Device.list().then((result) => {
+        res.render('device_list', { title: 'Device List', device_list: result })
+    })
 };
 
 // Display detail page for a specific Device.
-exports.device_detail = function(req, res) {
-    //res.send('NOT IMPLEMENTED: Device detail: ' + req.params.id);
-
-    async.parallel({
-
-        device: function(callback) {
-            Device.findById(req.params.id)
-                .exec(callback);
-        },
-
-    }, function(err, results) {
-        if (err) { return next(err); }
-        if (results.device==null) { // No results.
-            var err = new Error('Device not found');
-            err.status = 404;
-            return next(err);
-        }
-        // Successful, so render
-        res.render('device_detail', { title: 'Device Detail', data: results.device} );
-    });
-
+exports.device_detail = function(req, res, next) {
+    Device.findById(req.params.id).then((result) => {
+        res.render('device_detail', { title: 'Device Detail', data: result} );
+    }).catch((e) => {
+        var err = new Error(e)
+        err.status = 404
+        return next(err)
+    })
 };
 
 // Display Device create form on GET.
@@ -89,93 +65,61 @@ exports.device_create_post = [
         }
         else {
             // Data from form is valid.
-            //console.log("Generating hash for password : " + req.body.password);
-            mosquittoPBKDF2.createPasswordAsync(req.body.password, (pbkdf2Password)=>{
-                //console.log("Hash created: " + pbkdf2Password);
+            var topicsdata = JSON.parse(req.body.topics);
 
-                    //var topicsdata = {}
-                    //topicsdata["public/#"] = "r";
-                    //topicsdata["device/" + req.body.type + "/" + req.body.name + "/#"] = "rw";
+            var devicedata = {
+                name: req.body.name,
+                type: req.body.type,
+                password: req.body.password,
+                topics: topicsdata,
+                superuser: req.body.superuser ? true : false
+            };
 
-                    var topicsdata = JSON.parse(req.body.topics);
-
-                    var devicedata = {
-                        name: req.body.name,
-                        type: req.body.type,
-                        password: pbkdf2Password,
-                        topics: topicsdata,
-                        superuser: req.body.superuser ? true : false
-                    };
-
-                    //devicedata['topics'] = topicsdata;
-
-                    // Create a Device object with escaped and trimmed data.
-                    var device = new Device(
-                        devicedata
-                    );
-
-                    device.save(function (err) {
-                        if (err) {
-                            return next(err);
-                        }
-                        // Successful - redirect to new device record.
-                        res.redirect(device.url);
-                    });
-
-                }
+            // Create a Device object with escaped and trimmed data.
+            var device = new Device.Device(
+                devicedata
             );
+
+            device.save().then(() => {
+                res.redirect(device.url);
+            }).catch((e) => {
+                var err = new Error(e)
+                return next(err);
+            })
         }
     }
 ];
 
 // Display Device delete form on GET.
-exports.device_delete_get = function(req, res) {
-    async.parallel({
-        device: function (callback) {
-            Device.findById(req.params.id).exec(callback)
-        },
-    }, function (err, results) {
-        if (err) { return next(err); }
-        if (results.device == null) { // No results.
-            res.redirect('/');
-        }
-        // Successful, so render.
-        res.render('device_delete', { title: 'Delete Device', device: results.device});
-    });
-
+exports.device_delete_get = function(req, res, next) {
+    Device.findById(req.params.id).then((result) => {
+        res.render('device_delete', { title: 'Delete Device', device: result});
+    }).catch((e) => {
+        var err = new Error(e)
+        err.status = 404
+        return next(err)
+    })
 };
 
 // Handle Device delete on POST.
-exports.device_delete_post = function(req, res) {
-    async.parallel({
-        device: function (callback) {
-            Device.findById(req.body.device_id).exec(callback)
-        },
-    }, function (err, results) {
-        if (err) { return next(err); }
-        // Success.
-        Device.findByIdAndRemove(req.body.device_id, function deleteDevice(err) {
-            if (err) { return next(err); }
-                // Success - go to device list.
-                res.redirect('/devices')
-            })
-
-    });
+exports.device_delete_post = function(req, res, next) {
+    Device.remove(req.body.device_id).then((result) => {
+        res.redirect('/devices')
+    }).catch((e) => {
+        var err = new Error(e)
+        return next(err)
+    })
 };
 
 // Display Device update form on GET.
-exports.device_update_get = function(req, res) {
-    Device.findById(req.params.id, function (err, device) {
-        if (err) { return next(err); }
-        if (device == null) { // No results.
-            var err = new Error('Device not found');
-            err.status = 404;
-            return next(err);
-        }
-        // Success.
-        res.render('device_form', { title: 'Update Device', device: device });
-
-    });
+exports.device_update_get = function(req, res, next) {
+    Device.findById(req.params.id).then((result) => {
+        res.render('device_form', { title: 'Update Device', device: result });
+    }).catch((e) => {
+        var err = new Error(e)
+        err.status = 404
+        return next(err)
+    })
 };
 
 // Handle Device update on POST.
@@ -197,25 +141,18 @@ exports.device_update_post = [
         // Extract the validation errors from a request.
         const errors = validationResult(req);
 
-        // Create Device object with escaped and trimmed data (and the old id!)
-        //var topicsdata = {}
-        //topicsdata["public/#"] = "r";
-        //topicsdata["device/" + req.body.type + "/" + req.body.name + "/#"] = "rw";
-
         var topicsdata = JSON.parse(req.body.topics);
 
         var devicedata = {
-            _id: req.params.id,
             name: req.body.name,
             type: req.body.type,
+            password: req.body.password,
             topics: topicsdata,
-            superuser: req.body.superuser ? true: false
+            superuser: req.body.superuser ? true : false
         };
 
-        //devicedata['topics'] = topicsdata;
-
         // Create a Device object with escaped and trimmed data.
-        var device = new Device(
+        var device = new Device.Device(
             devicedata
         );
 
@@ -226,20 +163,12 @@ exports.device_update_post = [
         }
         else {
             // Data from form is valid. Update the record.
-            mosquittoPBKDF2.createPasswordAsync(req.body.password, (pbkdf2Password)=> {
-                devicedata.password = pbkdf2Password;
-                var device = new Device(
-                    devicedata
-                );
-
-                Device.findByIdAndUpdate(req.params.id, device, {}, function (err, thedevice) {
-                    if (err) {
-                        return next(err);
-                    }
-                    // Successful - redirect to device detail page.
-                    res.redirect(thedevice.url);
-                });
-            });
+            device.save().then(() => {
+                res.redirect(device.url);
+            }).catch((e) => {
+                var err = new Error(e)
+                return next(err);
+            })
         }
     }
 
--- mqttadmin/models/device.js
+++ mqttadmin/models/device.js
@@ -1,22 +1,255 @@
-var mongoose = require('mongoose');
+const Validator = require('jsonschema').Validator;
+const Zookeeper = require('node-zookeeper-client');
+const scram = require('../libs/scram');
 
-var Schema = mongoose.Schema;
+async function count() {
+    return new Promise((resolve, reject) => {
+        var client = Zookeeper.createClient('host.docker.internal:2181');
 
-var DeviceSchema = new Schema(
-    {
-        name: {type: String, required: true},
-        type: {type: String},
-        password: {type: String, required: true},
-        topics: {type: Object},
-        superuser: {type: Boolean}
-    }
-);
-
-DeviceSchema
-    .virtual('url')
-    .get(function () {
-        return '/device/' + this._id;
+        client.once('connected', function () {
+            client.getChildren('/config/users', function(error, children) {
+                if (error) {
+                    reject(error);
+                }
+                resolve(children.length);
+                client.close();
+            });
+        });
+        client.connect();
     });
+}
 
-//Export model
-module.exports = mongoose.model('Device', DeviceSchema);
+async function list() {
+    return new Promise((resolve, reject) => {
+        var client = Zookeeper.createClient('host.docker.internal:2181');
+
+        client.once('connected', function () {
+            client.getChildren('/config/users', function(error, children) {
+                if (error) {
+                    reject(error);
+                }
+                entities = []
+                children.forEach((child) => {
+                    entities.push({"name": child, "type": "zookeeper", "url": "/device/" + child})
+                })
+                resolve(entities);
+                client.close();
+            });
+        });
+        client.connect();
+    });
+}
+
+async function findById(id) {
+    return new Promise((resolve, reject) => {
+        var client = Zookeeper.createClient('host.docker.internal:2181');
+        var promises = []
+
+        client.once('connected', function () {
+            client.getChildren('/kafka-acl/Topic', function(error, children) {
+                if (error) {
+                    reject(error);
+                }
+                children.forEach((child) => {
+                    console.log("getting /kafka-acl/Topic/%s", child);
+                    promises.push(new Promise((resolve, reject) => {
+                        client.getData('/kafka-acl/Topic/' + child, function(error, data) {
+                            acl = JSON.parse(data.toString('utf8'))
+                            if (error) {
+                                console.log("failed to get /kafka-acl/Topic/%s", child)
+                                console.log(error)
+                            }
+                            var topics = {}
+                            acl.acls.filter(acl => acl.principal === "User:" + id).forEach((rule) => {
+                                if (rule.operation !== "Read" && rule.operation !== "Write") {
+                                    return
+                                }
+                                if (child in topics) {
+                                    topics[child] = "rw"
+                                } else if (rule.operation === "Read") {
+                                    topics[child] = "r"
+                                } else if (rule.operation === "Write") {
+                                    topics[child] = "w"
+                                }
+                            })
+                            resolve(topics)
+                        })
+                    }))
+                })
+            })
+            client.getData('/config/users/' + id, function(error, data) {
+                if (error) {
+                    reject(error)
+                }
+                Promise.all(promises).then((values) => {
+                    var topics = {}
+                    values.forEach((topic) => {
+                        Object.assign(topics, topic)
+                    })
+                    client.close();
+                    resolve({"name": id, "type": "zookeeper", "url": "/device/" + id, "password": data.toString('utf8'), "topics": topics, "superuser": false})
+                })
+            });
+        });
+        client.connect();
+    })
+}
+
+async function remove(id) {
+    return new Promise((resolve, reject) => {
+        var client = Zookeeper.createClient('host.docker.internal:2181');
+
+        client.once('connected', function () {
+            client.remove('/config/users/' + id, function(error, data) {
+                if (error) {
+                    reject(error)
+                }
+                resolve()
+                client.close();
+            });
+        });
+        client.connect();
+    })
+}
+
+class Device {
+    constructor(data) {
+        var v = new Validator();
+        var schema = {
+            name: {type: String},
+            type: {type: String},
+            password: {type: String},
+            topics: {type: Object}, // { "public/#" : "r", "/device/bar/foo/#" : "rw" }
+            superuser: {type: Boolean},
+            required: ["name", "password"]
+        };
+        var result = v.validate(data, schema, {throwError: true});
+        Object.assign(this, result.instance);
+        this.url = "/device/" + this.name;
+        this.scramuser = new scram.SCRAM(this.password, 4096);
+        return this;
+    }
+
+    async updateacl(topics, name, topic) {
+        return new Promise((resolve, reject) => {
+            var client = Zookeeper.createClient('host.docker.internal:2181');
+            client.once('connected', function () {
+                console.log('Connected to the server.');
+                client.exists('/kafka-acl/Topic/' + topic, function(error, aclstat) {
+                    if (aclstat) {
+                        console.log('Topic \'/kafka-acl/Topic/' + topic + '\' exists, getting data');
+                        client.getData('/kafka-acl/Topic/' + topic, function(error, data) {
+                            var acl = JSON.parse(data.toString('utf8'))
+                            console.log(acl)
+                            if (topics[topic] === 'r' || topics[topic] === 'rw') {
+                                acl.acls.push({"principal": "User:" + name, "permissionType": "Allow", "operation": "Read", "host": "*"})
+                            } else if (topics[topic] === 'w' || topics[topic] === 'rw') {
+                                acl.acls.push({"principal": "User:" + name, "permissionType": "Allow", "operation": "Write", "host": "*"})
+                            }
+                            acl.acls.push({"principal": "User:" + name, "permissionType": "Allow", "operation": "Describe", "host": "*"})
+                            console.log(JSON.stringify(acl));
+                            client.setData('/kafka-acl/Topic/' + topic, Buffer.from(JSON.stringify(acl)), function(error, stat) {
+                                if (error) {
+                                    console.log('failed to set topic acl')
+                                    console.log(error)
+                                    client.close()
+                                    reject();
+                                }
+                                console.log('Node: /kafka-acl/Topic/%s is successfully created.', topic);
+                                client.close()
+                                resolve();
+                            })
+                        });
+                    } else {
+                        console.log('Topic \'/kafka-acl/Topic/' + topic + '\' doesn\'t exist, creating');
+                        var acl = {"version": 1, "acls": []}
+                        if (topics[topic] === 'r' || topics[topic] === 'rw') {
+                            acl.acls.push({"principal": "User:" + name, "permissionType": "Allow", "operation": "Read", "host": "*"})
+                        }
+                        if (topics[topic] === 'w' || topics[topic] === 'rw') {
+                            acl.acls.push({"principal": "User:" + name, "permissionType": "Allow", "operation": "Write", "host": "*"})
+                        }
+                        acl.acls.push({"principal": "User:" + name, "permissionType": "Allow", "operation": "Describe", "host": "*"})
+                        console.log(JSON.stringify(acl));
+                        client.create('/kafka-acl/Topic/' + topic, Buffer.from(JSON.stringify(acl)), function(error) {
+                            if (error) {
+                                console.log('failed to set topic acl')
+                                console.log(error)
+                                client.close()
+                                reject();
+                            }
+                            console.log('Node: /kafka-acl/Topic/%s is successfully created.', topic);
+                            client.close()
+                            resolve();
+                        })
+                    }
+                });
+            })
+            client.connect();
+        })
+    }
+
+    async save() {
+        return new Promise((resolve, reject) => {
+            var client = Zookeeper.createClient('host.docker.internal:2181');
+            var path = "/config/users/" + this.name;
+            var name = this.name;
+            var type = this.type;
+            var topics = this.topics;
+            var scramuser = this.scramuser;
+            var promises = []
+            var updateacl = this.updateacl
+
+            client.once('connected', function () {
+                console.log('Connected to the server.');
+
+                client.exists(path, function(error, stat) {
+                    if (stat) {
+                        client.setData(path, Buffer.from(JSON.stringify(scramuser)), function (error) {
+                            if (error) {
+                                console.log('Failed to create node: %s due to: %s.', path, error);
+                                client.close();
+                                reject(error);
+                            } else {
+                                console.log('Node: %s is successfully created.', path);
+                                client.close();
+                                resolve();
+                            }
+                        });
+                    } else {
+                        promises.push(new Promise((innerResolve, innerReject) => {
+                            client.create(path, Buffer.from(JSON.stringify(scramuser)), function (error) {
+                                if (error) {
+                                    console.log('Failed to create node: %s due to: %s.', path, error);
+                                    innerReject(error);
+                                } else {
+                                    console.log('Node: %s is successfully created.', path);
+                                    innerResolve()
+                                }
+                            });
+                        }))
+
+                        for (var topic in topics) {
+                            console.log(topic)
+                            promises.push(updateacl(topics, name, topic))
+                        }
+                        Promise.all(promises).then(() => {
+                            client.close();
+                            resolve()
+                        })
+                    }
+                });
+            });
+
+            client.connect();
+        })
+    }
+};
+
+module.exports = {
+    Device,
+    count,
+    list,
+    findById,
+    remove,
+};
