1374107888408c17a08b15a8299610860518f74b
András Maróy <andras@maroy.hu>
2020-05-13 18:06:32 +0200

    Replace mosquitto with kafka

--- mosquitto-auth-plugin/docker-entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#causes the shell to exit if any subcommand or pipeline returns a non-zero status
-set -e
-#the shell running as pid 1 will replace itself with the command from the command line arguments
-exec "$@"
-
