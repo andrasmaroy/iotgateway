04ae514b20df45e86c885160c4dc845c07265c2a
András Maróy <andras@maroy.hu>
2020-05-13 18:06:48 +0200

    Implement changes for kafka in Logger

--- logger/logger.py
+++ logger/logger.py
@@ -1,40 +1,32 @@
-import paho.mqtt.client as mqtt
+from kafka import KafkaConsumer
 import pymongo
 from datetime import datetime
-
-# The callback for when the client receives a CONNACK response from the server.
-def on_connect(client, userdata, flags, rc):
-    print("Connected to mauthp with result code "+str(rc))
-
-    # Subscribing in on_connect() means that if we lose the connection and
-    # reconnect then subscriptions will be renewed.
-    print("Subscribeing to #")
-    client.subscribe("#")
-    print("Subscribed to #")
-
-# The callback for when a PUBLISH message is received from the server.
-def on_message(client, userdata, msg):
-    print("on_message "+msg.topic+" "+str(msg.payload))
-    log_dict = { "datetime" : datetime.now(), "topic": msg.topic, "payload": str(msg.payload) }
-    #print("saving to logdb...")
-    mongo_client['logdb']['logs'].insert_one(log_dict)
-    print("saved to logdb "+msg.topic+" "+str(msg.payload))
 
 
 print("Connecting to mongodb://logdb:27017/")
 mongo_client = pymongo.MongoClient("mongodb://logdb:27017/")
 print("Connected to logdb")
 
-client = mqtt.Client()
-client.on_connect = on_connect
-client.on_message = on_message
+consumer = KafkaConsumer(
+        bootstrap_servers='broker:9092',
+        security_protocol='SASL_PLAINTEXT',
+        sasl_mechanism='SCRAM-SHA-256',
+        sasl_plain_username='iotgatewaylogger',
+        sasl_plain_password='iotgatewaylogpass')
+consumer.subscribe(pattern='.*')
+try:
+    while True:
+        msg = consumer.poll(0.1)
+        for message in consumer:
+            # message value and key are raw bytes -- decode if necessary!
+            # e.g., for unicode: `message.value.decode('utf-8')`
+            print("on_message "+message.topic+" "+str(message.value))
+            log_dict = {"datetime": datetime.now(), "topic": message.topic, "payload": str(message.value)}
+            mongo_client['logdb']['logs'].insert_one(log_dict)
+            print("saved to logdb "+message.topic+" "+str(message.value))
 
-client.username_pw_set("iotgatewaylogger", password="iotgatewaylogpass")
-print("Connecting to mauthp")
-client.connect("mauthp", 1883, 60)
+except KeyboardInterrupt:
+    pass
 
-# Blocking call that processes network traffic, dispatches callbacks and
-# handles reconnecting.
-# Other loop*() functions are available that give a threaded interface and a
-# manual interface.
-client.loop_forever()
+finally:
+    consumer.close()
